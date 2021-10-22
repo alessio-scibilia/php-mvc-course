62e0f97f2220e01e00ece87b2011112a1fd816b8
giudeppa <giu.deppa@gmail.com>
2021-10-22 10:24:29 +0200

    Impl: added style css for copyright

--- Views/backoffice.head.common.php
+++ Views/backoffice.head.common.php
@@ -1,5 +1,5 @@
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
@@ -8,7 +8,7 @@
     <!-- MCpiver library & Tema -->
     <link rel="stylesheet" href="/css/padding.css">
     <link rel="stylesheet" href="/css/margin.css">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/backoffice-main.css">
 
     <link href="/vendor/summernote/summernote.css" rel="stylesheet">
     <link href="/css/bootstrap-select.min.css" rel="stylesheet">
