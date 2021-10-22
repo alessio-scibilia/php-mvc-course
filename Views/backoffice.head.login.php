ae76e08d76b13cf55f69242dac7e179e89b3a54a
giudeppa <giu.deppa@gmail.com>
2021-10-22 10:15:53 +0200

    Impl: added style css to login

--- Views/backoffice.head.login.php
+++ Views/backoffice.head.login.php
@@ -1,5 +1,5 @@
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
@@ -12,6 +12,9 @@
     <link rel="stylesheet" href="/css/padding.css">
     <link rel="stylesheet" href="/css/margin.css">
     <link rel="stylesheet" href="/css/backoffice-main.css">
+    <link rel="stylesheet" href="/css/style_theme.css">
 
-    <style>body { background: #ebeef6; }</style>
+    <style>body {
+            background: #ebeef6;
+        }</style>
 </head>
