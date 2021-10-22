3d80da335686122a606ddddd53877afceb48ddd0
alessio-scibilia <alessio.scibilia@gmail.com>
2021-10-22 10:39:38 +0200

    Fix: return url in language selector

--- Views/backoffice.language.selector.php
+++ Views/backoffice.language.selector.php
@@ -1,7 +1,8 @@
 <div class="lang-select">
     <?php $currentLanguage = $view_model->languages->get_selected(); ?>
     <?php foreach ($view_model->languages->list_all() as $language) { ?>
-        <a href="/language?return_url=<?php echo urlencode($view_model->current_url); ?>&lang=<?php echo $language['abbreviazione']; ?>"
+        <a href="/language?lang=<?php echo $language['abbreviazione']; ?>"
+           onclick="window.location.assign($(this).attr('href') + '&return_url=' + window.location.pathname); return false;"
            class="<?php echo($language['id'] == $currentLanguage['id'] ? 'active-lang' : '') ?>">
             <?php echo $language['abbreviazione']; ?>
         </a>
