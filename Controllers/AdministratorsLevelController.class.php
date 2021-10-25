45938d3c07bd2230ff97c3201279dfc5887a23cd
alessio-scibilia <alessio.scibilia@gmail.com>
2021-10-25 16:16:09 +0200

    Fix: session writing

--- Controllers/AdministratorsLevelController.class.php
+++ Controllers/AdministratorsLevelController.class.php
@@ -7,13 +7,8 @@ require_once 'Views/HttpRedirectView.class.php';
 
 class AdministratorsLevelController
 {
-
-    /** @var UserRepository */
-    protected $user_repository;
-
     public function __construct()
     {
-        $this->user_repository = new UserRepository();
     }
 
     /**
@@ -24,60 +19,16 @@ class AdministratorsLevelController
     {
         $return_url = $params['return_url'] ?? '/backoffice';
         $user = SessionManager::get_user();
-
         if (User::is_empty($user) || $user->level != 0) //Solo gli utenti con livello God user possono cambiare il proprio livello a piacimento
         {
             return new HttpRedirectView('/backoffice');
         }
 
         $request = $params['request'];
-        switch ($request) {
-            case '0':
-                // Settare sessionManager o user o entrambi?
-                return new HttpRedirectView($return_url);
-                break;
-            case '1':
-                break;
+        $user->level = intval($request);
+        $user->level_name = Level::name($request);
+        SessionManager::set_user($user);
 
-            case '2':
-                break;
-
-            case '3':
-                break;
-
-            case '4':
-                break;
-
-            default:
-                return new HttpRedirectView('/backoffice/dashboard');
-
-
-                SessionManager::set_lang($language['id']);    //forse una cosa del genere è da fare sullo user?
-        }
+        return new HttpRedirectView($return_url);
     }
-
-    /*
-     * @param array $params
-     * @return HttpRedirectView
-     *
-    public function http_post(array &$params): HttpRedirectView
-    {
-        $email = $params['email'];
-        $password = $params['password'];
-
-        $hotel = $this->hotel_repository->get_by_email_password($email, $password);
-        if ($hotel == null) {
-            $user = $this->user_repository->get_by_email_password($email, $password);
-            if ($user == null) {
-                return new HttpRedirectView('/backoffice');
-            } else {
-                SessionManager::set_user(new User($user));
-                return new HttpRedirectView('/backoffice/dashboard');
-            }
-        } else {
-            SessionManager::set_user(new User($hotel));
-            return new HttpRedirectView('/backoffice/dashboard');
-        }
-    }
-    */
 }
