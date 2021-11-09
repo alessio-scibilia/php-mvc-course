b0a6bfde8427654e850c318eae51f0f94b2a61e7
giudeppa <giu.deppa@gmail.com>
2021-11-09 22:20:37 +0100

    Impl: View and controller of backend refactoring

---
+++ Controllers/BackofficeGuestsEditController.class.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'Database/UserRepository.class.php';
+require_once 'Database/GuestRepository.class.php';
+require_once 'Database/LanguageRepository.class.php';
+require_once 'Database/TranslationRepository.class.php';
+require_once 'Database/UserRepository.class.php';
+require_once 'Middlewares/SessionManager.class.php';
+require_once 'Models/User.class.php';
+require_once 'ViewModels/BackOfficeViewModel.class.php';
+require_once 'Views/HttpRedirectView.class.php';
+require_once 'Views/HtmlView.class.php';
+require_once 'Views/Html404.class.php';
+
+class BackofficeGuestsEditController
+{
+    protected $user_repository;
+    protected $language_repository;
+    protected $translation_repository;
+    protected $guest_repository;
+
+    public function __construct()
+    {
+        $this->user_repository = new UserRepository();
+        $this->language_repository = new LanguageRepository();
+        $this->translation_repository = new TranslationRepository();
+        $this->guest_repository = new GuestRepository();
+    }
+
+    public function http_get(array &$params): IView
+    {
+        if (isset($params['guests'])) {
+            $user = SessionManager::get_user();
+
+            // Solo gli utenti con level <= 2 possono accedere a queste pagine "amministratori",
+            // Gli altri bisogna mandarli su pagine adeguate tramite redirect
+            if (User::is_empty($user) || $user->level > 2) {
+                return new HttpRedirectView('/backoffice');
+            }
+
+            $languages = new Languages($this->language_repository->list_all());
+            $id_lingua = SessionManager::get_lang();
+            $languages->select($id_lingua);
+
+            $translations = new Translations($this->translation_repository->list_by_language($id_lingua));
+            $title = $translations->get('gestione_ospiti') . ' | ' . $translations->get('nome_sito');
+
+            $id = intval($params['guests']);
+            $administrator = $this->guest_repository->get_by_id($id);
+
+            $view_model = new BackOfficeViewModel('backoffice.guests.edit', $title, $languages, $translations);
+            $view_model->user = $user;
+            $view_model->admin = $administrator;
+            $view_model->menu_active_btn = 'guests';
+
+            return new HtmlView($view_model);
+        }
+
+        return new HttpRedirectView('/backoffice/guests');
+    }
+}
--- Views/backoffice.facilities.categories.edit.php
+++ Views/backoffice.facilities.categories.edit.php
@@ -1,1 +1,72 @@
-<?php
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 d-flex align-items-center justify-content-start mb15">
+            <a href="javascript:void()" id="gobacksearch" class="open-view-action-inside back-btn"
+               data-action="<?php echo $langs['link_strutture']; ?>"
+               data-title="<?php echo $langs['gestione_strutture']; ?>" data-params="<?php echo $langs['categorie']; ?>"
+               data-search="<?php if (isset($search_val)) echo $search_val; ?>"><i
+                        class="fa fa-angle-left"></i> <?php echo $langs['lista_categorie']; ?> /</a>
+            <h1><i class="fa fa-list"></i> <?php echo $view_model->translations->get('modifica_categoria'); ?></h1>
+        </div>
+
+        <div class="col-xl-8 col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title"><?php echo $view_model->translations->get('dati_categoria'); ?></h4>
+                </div>
+                <div class="card-body">
+                    <div class="basic-form">
+
+                        <?php
+                        $lingue = $view_model->languages->list_all();
+                        for ($x = 0; $x < sizeof($lingue); $x++) {
+                            //$cat_trad = getCategoria($dbh,$params[2],$lingue[$x]['shortcode_lingua']);
+                            ?>
+                            <div class="form-group row">
+
+                                <label class="col-sm-3 col-form-label"><?php echo $langs['nome']; ?><?php echo $lingue[$x]['abbreviazione']; ?></label>
+                                <div class="col-sm-9">
+
+
+                                    <input value="<?php echo //$cat_trad['nome'];?>" type="text"
+                                           class="nome_cat form-control validate-1"
+                                           id="nome_<?php //echo $lingue[$x]['shortcode_lingua'];?>"
+                                           placeholder="Sport">
+                                </div>
+                            </div>
+                        <?php } ?>
+                        <input type="hidden" name="image_path" value="<?php echo //$cat_trad['immagine'];?>"
+                               id="img_path" class="validate-1">
+                        <input type="hidden" id="id_cat" value="<?php echo //$cat_trad['related_id'];?>"
+                               class="validate-1">
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label"><?php echo $view_model->translations->get('immagine_categoria'); ?></label>
+                            <div class="col-sm-9 custom-file">
+                                <input type="file" data-function="addCategory" class="custom-file-input"
+                                       id="immagini_cat">
+                                <label class="custom-file-label"><?php echo $view_model->translations->get('scegli_immagine'); ?></label>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-sm-12">
+                                <div class="input-group col-md-12" id="preview-img-container">
+                                    <div id="preview">
+                                        <div class="img-form-preview ">
+                                            <img class="img-form-preview-item"
+                                                 src="<?php //echo $cat_trad['immagine'];?>" height="200px">
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-primary validate-it" id="validate-1"
+                                        data-params="false"
+                                        data-function="updateCategory"><?php echo $view_model->translations->get('salva'); ?></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
