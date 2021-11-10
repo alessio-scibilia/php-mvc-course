c434c7d75385f0d3c6b2f653879fe8c6c0a2273f
alessio-scibilia <alessio.scibilia@gmail.com>
2021-11-10 08:50:54 +0100

    Impl: included frontoffice main js

--- Views/frontoffice.footer.php
+++ Views/frontoffice.footer.php
@@ -2,5 +2,5 @@
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 <script type="text/javascript" src="/js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="/js/main.js"></script>
+<script type="text/javascript" src="/js/frontoffice-main.js"></script>
 
