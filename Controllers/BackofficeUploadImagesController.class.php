38f8f011bc29af8e07ea11d9af313719ddd8a5ae
giudeppa <giu.deppa@gmail.com>
2021-11-10 13:14:07 +0100

    Fix: upload images fixing

--- Controllers/BackofficeUploadImagesController.class.php
+++ Controllers/BackofficeUploadImagesController.class.php
@@ -22,7 +22,7 @@ class BackofficeUploadImagesController
         $this->user_repository = new UserRepository();
     }
 
-    public function http_get(array &$params): IView
+    public function http_post(array &$params): IView
     {
         if (isset($params['administrators'])) {
             return new JsonView(false);
@@ -33,7 +33,50 @@ class BackofficeUploadImagesController
                 return new JsonView(false);
             }
 
-            return new JsonView($params);
+
+            // Count total files
+            $countfiles = count($params['immagini_form']['name']);
+
+// Upload directory
+            $upload_location = $params['DOCUMENT_ROOT'] . "/uploads/";
+
+// To store uploaded files path
+            $files_arr = array();
+
+// Loop all files
+            for ($index = 0; $index < $countfiles; $index++) {
+
+                if (isset($params['immagini_form']['name'][$index]) && $params['immagini_form']['name'][$index] != '') {
+                    // File name
+                    $filename_backup = $params['immagini_form']['name'][$index];
+
+                    $randomNumber = rand(0, 10000000000);
+
+                    for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1; $i != 32; $x = rand(0, $z), $s .= $a{$x}, $i++) ;
+
+                    $filename = $randomNumber . $s;
+
+                    // Get extension
+                    $ext = strtolower(pathinfo($filename_backup, PATHINFO_EXTENSION));
+
+                    // Valid image extension
+                    $valid_ext = array("png", "jpeg", "jpg");
+
+                    // Check extension
+                    if (in_array($ext, $valid_ext)) {
+
+                        // File path
+                        $path = $upload_location . $filename . '.' . $ext;
+
+                        // Upload file
+                        if (move_uploaded_file($params['immagini_form']['tmp_name'][$index], $path)) {
+                            $files_arr[] = 'uploads/' . $filename . '.' . $ext;
+                        }
+                    }
+                }
+            }
+
+            return new JsonView($files_arr);
         }
     }
 }
