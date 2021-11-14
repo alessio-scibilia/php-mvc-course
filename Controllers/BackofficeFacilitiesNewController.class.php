138ca65f5e4bb06b77aedf33d94bf6b6bb956c83
giudeppa <giu.deppa@gmail.com>
2021-11-14 20:29:41 +0100

    Fix: Fix facilities categories edit and facilities create refactoring

--- Controllers/BackofficeFacilitiesNewController.class.php
+++ Controllers/BackofficeFacilitiesNewController.class.php
@@ -43,56 +43,52 @@ class BackofficeFacilitiesNewController
 
     public function http_get(array &$params): IView
     {
-        if (isset($params['facilities'])) {
-            $languages = new Languages($this->language_repository->list_all());
-            $id_lingua = SessionManager::get_lang();
-            $languages->select($id_lingua);
-            $language = $languages->get($id_lingua);
+        $languages = new Languages($this->language_repository->list_all());
+        $id_lingua = SessionManager::get_lang();
+        $languages->select($id_lingua);
+        $language = $languages->get($id_lingua);
 
-            $translations = new Translations($this->translation_repository->list_by_language($id_lingua));
-            $title = $translations->get('gestione_eventi') . ' | ' . $translations->get('nome_sito');
+        $translations = new Translations($this->translation_repository->list_by_language($id_lingua));
+        $title = $translations->get('gestione_eventi') . ' | ' . $translations->get('nome_sito');
 
-            $user = SessionManager::get_user();
-            if (User::is_empty($user)) {
-                return new HttpRedirectView('/backoffice');
-            }
-
-            $facilities = new Facility();
-            $principal = $facilities;
-
-            if ($user->level <= 2) {
-                $rows = $this->hotel_repository->get_all_hotels($language['shortcode_lingua']);
-                $hotels = Hotel::hotels($rows);
-
-                $rows = $this->category_repository->get_all_categories($language['shortcode_lingua']);
-                $categories = Category::categories($rows);
-            } else {
-                $hotels = array();
-                $categories = array();
-            }
-
-            $rows = $this->facility_hotel_repository->get_related_by_facility_id_and_language_id($principal->related_id, $language['shortcode_lingua']);
-            $related_hotels = Hotel::hotels($rows);
-
-            $rows = $this->category_repository->get_by_facility($principal->related_id, $language['shortcode_lingua']);
-            $related_categories = Category::categories($rows);
-
-
-            $view_model = new BackOfficeViewModel('backoffice.facilities.edit', $title, $languages, $translations);
-            $view_model->user = $user;
-            $view_model->language = $language;
-            $view_model->facilities = $facilities; // all available languages
-            $view_model->principal = $principal;
-            $view_model->hotels = $hotels;
-            $view_model->categories = $categories;
-            $view_model->related_hotels = $related_hotels;
-            $view_model->related_categories = $related_categories;
-
-            $view_model->menu_active_btn = 'facilities';
-
-            return new HtmlView($view_model);
+        $user = SessionManager::get_user();
+        if (User::is_empty($user)) {
+            return new HttpRedirectView('/backoffice');
         }
 
-        return new HttpRedirectView('/backoffice/facilities');
+        $facilities = new Facility();
+        $principal = $facilities;
+
+        if ($user->level <= 2) {
+            $rows = $this->hotel_repository->get_all_hotels($language['shortcode_lingua']);
+            $hotels = Hotel::hotels($rows);
+
+            $rows = $this->category_repository->get_all_categories($language['shortcode_lingua']);
+            $categories = Category::categories($rows);
+        } else {
+            $hotels = array();
+            $categories = array();
+        }
+
+        $rows = $this->facility_hotel_repository->get_related_by_facility_id_and_language_id($principal->related_id, $language['shortcode_lingua']);
+        $related_hotels = Hotel::hotels($rows);
+
+        $rows = $this->category_repository->get_by_facility($principal->related_id, $language['shortcode_lingua']);
+        $related_categories = Category::categories($rows);
+
+
+        $view_model = new BackOfficeViewModel('backoffice.facilities.edit', $title, $languages, $translations);
+        $view_model->user = $user;
+        $view_model->language = $language;
+        $view_model->facilities = $facilities; // all available languages
+        $view_model->principal = $principal;
+        $view_model->hotels = $hotels;
+        $view_model->categories = $categories;
+        $view_model->related_hotels = $related_hotels;
+        $view_model->related_categories = $related_categories;
+
+        $view_model->menu_active_btn = 'facilities';
+
+        return new HtmlView($view_model);
     }
 }
