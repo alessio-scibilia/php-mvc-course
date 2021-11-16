472121abed52712446810076b1723233acbb382b
alessio-scibilia <alessio.scibilia@gmail.com>
2021-11-16 15:20:52 +0100

    Add: swift mailer

--- Middlewares/MailSender.class.php
+++ Middlewares/MailSender.class.php
@@ -5,28 +5,32 @@ class MailSender
 
     public static function send(string $from, string $to, string $bcc, string $obj, string $subject, string $body): string
     {
+        $result = mail($to, $subject, $message);
 
-        require_once 'vendor/autoload.php';
+        if (!$result)
+        {
+            require_once 'vendor/autoload.php';
 
-        //Parametri connessione server di posta
-        $email = 'auto@alfiere.digital';
-        $pwd = 'Nonteladico';
-        $server = 'ssl0.ovh.net';
-        $port = 465;
+            //Parametri connessione server di posta
+            $email = 'auto@alfiere.digital';
+            $pwd = 'Nonteladico';
+            $server = 'ssl0.ovh.net';
+            $port = 465;
 
-        $transport = (new Swift_SmtpTransport($server, $port))
-            ->setUsername($email)
-            ->setPassword($pwd);
-        $mailer = new Swift_Mailer($transport);
+            $transport = (new Swift_SmtpTransport($server, $port))
+                ->setUsername($email)
+                ->setPassword($pwd);
+            $mailer = new Swift_Mailer($transport);
 
-        $message = (new Swift_Message($obj))
-            ->setFrom($from)
-            ->setTo($to)
-            ->setBody($body);
+            $message = (new Swift_Message($obj))
+                ->setFrom($from)
+                ->setTo($to)
+                ->setBody($body);
 
-        $result = $mailer->send($message);
+            $result = $mailer->send($message);
 
-        return $result;
+            return $result;
+        }
     }
 }
 
