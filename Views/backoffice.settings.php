07265d3c21be803c9203291a27f8173ea4393e2a
alessio-scibilia <alessio.scibilia@gmail.com>
2021-11-16 11:21:10 +0100

    Fix: hotel creation with utilities

--- Views/backoffice.settings.php
+++ Views/backoffice.settings.php
@@ -4,7 +4,7 @@
             <h1><i class="fa fa-gear"></i> <?php echo $view_model->translations->get('impostazioni'); ?></h1>
         </div>
         <div class="col-md-12">
-            <pre><?php echo json_encode($_COOKIE, JSON_PRETTY_PRINT); ?></pre>
+            <!--pre><?php echo json_encode($_COOKIE, JSON_PRETTY_PRINT); ?></pre-->
         </div>
         <div class="col-xl-6 col-lg-12">
             <div class="card">
